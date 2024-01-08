eb27cc19c718ef9d86af64678a1ec88af1187590
Alekssyla <alekssyla2@gmail.com>
2024-01-08 10:35:25 +0100

    -

---
+++ index.js
@@ -0,0 +1,16 @@
+let email = "gjergjcanco@gmail.com";
+console.log(email);
+
+let firstName = "Aleks";
+let lastName = "syla";
+let fullName = firstName + lastName;
+console.log(fullName);
+console.log("My sitring length is: ", fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName);
+
+let index = email.indexOf("@");
+console.log(index);
+
+let result = email.lastIndexOf("g");
+console.log(result);
